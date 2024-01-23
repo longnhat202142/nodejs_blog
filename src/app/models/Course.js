fa8ba810ddbb52db438767a8cea18c599d18aecc
longnhat202142 <longnhat03082002@gmail.com>
2024-01-23 11:26:55 +0700

    Update course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
+    name: { type: String },
     description: { type: String, maxLength: 255 },
     image: { type: String, maxLength: 600 },
+    videoID: { type: String, maxLength: 600 },
     createAt: { type: Date, default: Date.now },
     updateAt: { type: Date, default: Date.now },
+    slug: { type: String, slug: 'name', unique: true },
 });
 
 module.exports = mongoose.model('Course', Course);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 const path = require('path');
+const methodOverride = require('method-override');
 
 const db = require('./config/db');
 const route = require('./routes');
 
 //Connect to DB;
 db.connect();
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+app.use(express.urlencoded({ extended: true }));
 //HTTP logger
 app.use(morgan('combined'));
 
@@ -21,6 +26,9 @@ app.engine(
     'hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
